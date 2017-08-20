1aba8ab8b161984b36b67f97130a2518553186f3
Annick <annick@macbook-pro-de-annick.home>
2017-08-20 10:04:52 +0200

    work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { AuthenticationModule } from './authentication/authentication.module';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FlexLayoutModule,
+    AuthenticationModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
